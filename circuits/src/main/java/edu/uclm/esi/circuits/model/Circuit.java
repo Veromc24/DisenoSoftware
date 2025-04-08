2959d2dd4753088a163d81ddda5dafe489ba2d23
Carlos Ruiz García-Casarrubios <carlos.ruiz30@alu.uclm.es>
2025-04-08 16:45:55 +0200

    Co-authored-by: vicendavid24 <vicendavid24@users.noreply.github.com>

--- circuits/src/main/java/edu/uclm/esi/circuits/model/Circuit.java
+++ circuits/src/main/java/edu/uclm/esi/circuits/model/Circuit.java
@@ -1,26 +1,47 @@
 package edu.uclm.esi.circuits.model;
 
-public class Circuit {
-    private int outputQubits;
-    private int[][] table;
+import java.util.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import jakarta.persistence.Column;
 
+@Entity
+public class Circuit {
+    @Id @Column(length=36)
+    private String id;
+    private int outputQubits;
+    @Transient
+    private int[][] table;
+    @Column(length=50)
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
     public int getOutputQubits() {
         return outputQubits;
     }
-
+    public void setOutputQubits(int outputQubits) {
+        this.outputQubits = outputQubits;
+    }
     public int[][] getTable() {
         return table;
     }
-
-    public void setOutputQubits(int outputQubits) {
-                this.outputQubits = outputQubits;
-        }
-    
     public void setTable(int[][] table) {
-           this.table = table;
+        this.table = table;
     }
 
-    public String generateCode() {
-        return "hola";
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Circuit() {
+        this.id = java.util.UUID.randomUUID().toString();
     }
 }
--- circuits/src/main/java/edu/uclm/esi/circuits/services/CircuitService.java
+++ circuits/src/main/java/edu/uclm/esi/circuits/services/CircuitService.java
@@ -4,20 +4,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
 import edu.uclm.esi.circuits.model.Circuit;
 
 @Service
 public class CircuitService {
-public String createCircuit(Circuit circuit) {
 
-       return "hola";
+   @Autowired
+   private CircuitService service;
+   
+   @PostMapping("/createCircuit")
+   
+  public String createCircuit(@RequestBody Map<String, Object> body) {
+        if (!body.containsKey("table") || !body.containsKey("outputQubits")) {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "The request body must contain qubits and outputQubits");
+        }
+        return this.service.createCircuit(body);
+      }
 
-    }
+   @PostMapping("/generateCode")
 
-    public String generateCode(Circuit circuit) {
-
-        return circuit.generateCode();
- 
-     }
+   public String generateCode(@RequestParam(required=false) String name, @RequestBody Circuit circuit) {
+        if(name != null) {
+            circuit.setName(name);
+        }
+      
+        return this.service.generateCode(name, circuit);  
+  }
 }
