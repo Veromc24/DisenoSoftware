2e0f0402fef33ded6d4ce3ec41e3001f1b4faf57
Carlos Ruiz García-Casarrubios <carlos.ruiz30@alu.uclm.es>
2025-04-16 13:30:07 +0200

    switch to nostandalone angular proyect

---
+++ FEcircuits/circuitsfe/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CircuitComponent } from './circuit/circuit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CircuitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FEcircuits/circuitsfe/src/app/circuit/circuit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circuit',
+  standalone: false,
+  templateUrl: './circuit.component.html',
+  styleUrl: './circuit.component.css'
+})
+export class CircuitComponent {
+
+}
